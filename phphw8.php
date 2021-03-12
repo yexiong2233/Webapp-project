98fd9960339ba0ebb8b88da04bac57357ebd55c6
yexiong2233 <s19T15916@stu.edu.tw>
2021-03-12 17:04:35 +0800

    add phphw8.php

---
+++ phphw8.php
@@ -0,0 +1,26 @@
+<?php
+  $a = 10;
+  $b = 20;
+  echo '$a = 10'."\n";
+  echo '$b = 20'."\n";
+  echo '$a + $b = '.($a + $b)."\n";
+  echo '$a - $b = '.($a - $b)."\n";
+  echo '$a * $b = '.($a * $b)."\n";
+  echo '$a / $b = '.($a / $b)."\n";
+  echo '$a++ = '.($a++)."\n";
+  echo '$a = '.($a)."\n";
+  echo '++$a = '.(++$a)."\n";
+  echo '$a = '.($a)."\n";
+  echo '$b % $a = '.($b%$a)."\n";
+  echo '$a > $b '.($a > $b ? 'true' : 'false')."\n";
+  echo '$a += 1 ==> '.($a += 1)."\n";
+  echo '$a = '.($a)."\n";
+  echo '$a *= $b ==> '.($a *= $b)."\n";
+  echo '$a = '.($a)."\n";
+  echo '$a and $b ==> '.($a and $b)."\n";
+  echo '$a or $b ==> '.($a or $b)."\n";
+
+  echo 1<=>1;
+  echo 1<=>2;
+  echo 2<=>1;
+?>
