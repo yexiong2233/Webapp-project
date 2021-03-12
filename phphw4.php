364189304893db26f06ab0a754ef734196c803c0
yexiong2233 <s19T15916@stu.edu.tw>
2021-03-12 15:36:40 +0800

    add phphw4,php

---
+++ phphw4.php
@@ -0,0 +1,13 @@
+<?php
+    define('PI',3.1415926);
+
+    $radius = 10;
+    $area = $radius*$radius*PI;
+    echo "circule area is".$area."\n";
+
+    $length = $radius * 2 * PI;
+    echo "Circule length is ".$length."\n";
+
+
+
+?>
