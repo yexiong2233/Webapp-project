85e1f15acd7897fbc0a0291b5541009df4b58989
yexiong2233 <s19T15916@stu.edu.tw>
2021-03-19 19:25:23 +0800

    add

--- ex2.php
+++ ex2.php
@@ -1,9 +1,11 @@
 <?php
-//$i=10;
-//echo($i<20)?"大":"小";
-  $input = isset($value=)? $value : "空的";
-  echo $input."\n";
-  $input = $value ?? "空的";
-  echo $input;
+$i=10;
+echo($i<20)?"大":"小";
+
+
+// $input = isset($value=)? $value : "空的";
+  //echo $input."\n";
+  //$input = $value ?? "空的";
+  //echo $input;
 
 ?>
