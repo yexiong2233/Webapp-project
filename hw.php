48e900fb636655b9780ba4c80d63e82ec36d7517
yexiong2233 <s19T15916@stu.edu.tw>
2021-03-19 19:24:30 +0800

    add

---
+++ hw.php
@@ -0,0 +1,21 @@
+
+<?php
+$score ="40";
+switch(intval($score/10)){
+case 9;
+echo "A";
+break;
+case 8;
+echo "B";
+break;
+case 7;
+echo"c";
+case 6;
+echo "D";
+break;
+default;
+echo"E";
+
+}
+
+?>
