e394f69461aa221c77206eb77f5b03775987c448
yexiong2233 <s19T15916@stu.edu.tw>
2021-03-19 14:41:38 +0800

    add webapp

---
+++ eggbox.php
@@ -0,0 +1,18 @@
+<?php
+$eggbox = array();
+$eggbox["土雞"]=10;
+$eggbox[]=20;
+$eggbox[4]=21;
+array_push($eggbox,"Apple");
+$eggbox[2]="pineapple";
+print_r($eggbox);
+array_pop($eggbox);
+print_r($eggbox);
+array_pop($eggbox);
+print_r($eggbox);
+$eggbox=array(10,20,21);
+print_r($eggbox);
+
+print_r($eggbox);
+
+?>
